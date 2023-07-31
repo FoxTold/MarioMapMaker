05e1543148c7c6b50c9a42de523ae09632834912
Tomasz Rybinski <rybawoda2@gmail.com>
2023-08-01 01:17:21 +0200

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+void initializeGrid(const std::vector < sf::RectangleShape > cells)
+{
 
+}
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::RenderWindow window(sf::VideoMode(1500, 400), "SFML works!");
+    std::vector<sf::RectangleShape> gridCells = {};
+    for (float i = 0; i < 400; i += 10)
+    {
+        for (float j = 0; j < 1200; j += 10)
+        {
+            auto shape = sf::RectangleShape(sf::Vector2f(10.f, 10.f));
+            shape.setPosition(j, i);
+            shape.setFillColor(sf::Color::Blue);
+            gridCells.push_back(shape);
+        }
+    }
+    std::vector< std::vector<sf::Vertex>> gridLines = {};
+    for (float i = 1; i <= 401; i += 10)
+    {
+        gridLines.push_back({
+            sf::Vertex(sf::Vector2f(0, i),sf::Color::White),
+            sf::Vertex(sf::Vector2f(1200, i),sf::Color::White)
+            });
+    }
+    for (float i = 1; i <= 1201; i += 10)
+    {
+        gridLines.push_back({
+            sf::Vertex(sf::Vector2f(i, 0),sf::Color::White),
+            sf::Vertex(sf::Vector2f(i, 400),sf::Color::White)
+            });
+    }
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,7 +43,14 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for (const auto& x : gridCells)
+        {
+            window.draw(x);
+        }
+        for (const auto& x : gridLines)
+        {
+            window.draw(x.data(), 2, sf::Lines);
+        }
         window.display();
     }
 
